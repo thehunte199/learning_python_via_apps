3e7da9a4f7dc72dd0755237b71a51efccc4412ec
James Ward <jward3210@gmail.com>
2019-12-24 15:01:23 -0500

    updated my version of the application, removed zip folder, and placed test hosts file directly in app directory

--- App3_Website-Blocker/my-website-blocker.py
+++ App3_Website-Blocker/my-website-blocker.py
@@ -1,16 +1,32 @@
 import time
 from datetime import datetime as dt
 
-hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
+hosts_path ="hosts"
+# hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
 redirect = "127.0.0.1"
 websites = ["www.facebook.com", "facebook.com", "www.youtube.com", "youtube.com", "www.reddit.com", "reddit.com"]
-
-counter = 0
+currently_worktime = None
 
 while True:
-    print("It is:")
-    if dt(dt.now().hour, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().hour, dt.now().month, dt.now().day, 8):
-        print("Work time")
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 12):
+        if currently_worktime == False or currently_worktime == None:
+            currently_worktime = True
+            with open(hosts_path, 'r+') as file:
+                content = file.read()
+                for website in websites:
+                    if website in content:
+                        pass
+                    else:
+                        file.write(redirect+" "+website+"\n")
+            
     else:
-        print("Fun time")
+        if (currently_worktime == True or currently_worktime == None):
+            currently_worktime = False
+            with open(hosts_path, 'r+') as file:
+                content = file.readlines()
+                file.seek(0)
+                for line in content:
+                    if not any(website in line for website in websites):
+                        file.write(line)
+                file.truncate()
     time.sleep(2.5)
